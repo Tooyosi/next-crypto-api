1ee9c272f313bcf0315b108d844c5aeddd57782e
Oluwatoyosi Oyegoke <oluwatoyosi.oyegoke@gmail.com.com>
2020-04-09 08:52:24 +0100

    fitst commit with app setup

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const {logger} = require('./loggers/logger')
+const path = require("path")
+const swaggerUi = require('swagger-ui-express');
+const specs = require('./swagger/index')
+require('dotenv').config()
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+
+app.get('/swagger.json', function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(specs);
+});
+
+app.listen(process.env.PORT, ()=>{
+    logger.info(`Application is running on port ${process.env.PORT}`)
+})
